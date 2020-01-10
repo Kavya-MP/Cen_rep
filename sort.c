e97175abb195210f5c2bb3de7c42f9b7fb76b042
Kavya-MP <59682943+Kavya-MP@users.noreply.github.com>
2020-01-10 06:47:29 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,26 +1,50 @@
 
 #include <stdio.h>
-#include <string.h>
+#define MAX_SIZE 100    // Maximum array size
 
- sort (void) {
-   char string[] = "simplyeasylearning";
-   char temp;
+sort()
+{
+    int arr[MAX_SIZE];
+    int size;
+    int i, j, temp;
 
-   int i, j;
-   int n = strlen(string);
+    /* Input size of array */
+    printf("Enter size of array: ");
+    scanf("%d", &size);
 
-   printf("String before sorting - %s \n", string);
+    /* Input elements in array */
+    printf("Enter elements in array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
 
-   for (i = 0; i < n-1; i++) {
-      for (j = i+1; j < n; j++) {
-         if (string[i] > string[j]) {
-            temp = string[i];
-            string[i] = string[j];
-            string[j] = temp;
-         }
-      }
-   }
-   
-   printf("String after sorting  - %s \n", string);
- //  return 0;
-}
+    for(i=0; i<size; i++)
+    {
+        /*
+         * Place currently selected element array[i]
+         * to its correct place.
+         */
+        for(j=i+1; j<size; j++)
+        {
+            /*
+             * Swap if currently selected array element
+             * is not at its correct position.
+             */
+            if(arr[i] > arr[j])
+            {
+                temp     = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    /* Print the sorted array */
+    printf("\nElements of array in ascending order: ");
+    for(i=0; i<size; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+
+ //   return 0;
